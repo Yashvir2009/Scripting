7890fe35367f2e786f98428aefbc71431b8e37e1
Yashvir2009 <160936176+Yashvir2009@users.noreply.github.com>
2024-02-22 19:06:46 +0000

    Update HelloWorld.cs

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -7,12 +7,12 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print("crickets!");
     }
 }
